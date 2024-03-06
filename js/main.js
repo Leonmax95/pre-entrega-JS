ca3b5cce07a0f25b4ad52ab869a42d84d1fa1d37
leonmax <belizanandres393@gmail.com>
2024-03-05 22:25:40 -0300

    2da re-entrega

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 let nombreUsuario = prompt("Ingrese su nombre por favor:");
 alert("Bienvenido " + nombreUsuario + ".");
 
-// Precios de prendas
+// precios de prendas
 const precioVestido = 12000;
 const precioBlusa = 9000;
 const precioPantalon = 13500;
 const precioRemera = 18000;
 const precioCartera = 23000;
 
-let totalCompra = 0; // Almacenar el total de la compra
+let totalCompra = 0; // almacenar el total de la compra
 
-// Función para determinar el nombre de la prenda seleccionada
+// función para determinar el nombre de la prenda seleccionada
 function determinarPrenda(opcion) {
     let prendaElegida;
     switch (opcion) {
@@ -42,10 +42,25 @@ do {
     let opcion = parseInt(prompt("Seleccione la prenda que desea comprar: \n1. Vestidos \n2. Blusas \n3. Pantalones \n4. Remeras \n5. Carteras"));
     let cantidad = parseInt(prompt("Ingrese la cantidad de prendas que desea comprar:"));
 
+    if (isNaN(cantidad) || cantidad <= 0) {
+        alert("Por favor, ingrese una cantidad válida y mayor que cero.");
+        continue; // vuelve al inicio del bucle para seleccionar nuevamente
+    } else if (cantidad > 50) { // limite 50 unidades
+        let confirmarCantidad = confirm("Ha ingresado una cantidad muy alta. ¿Desea continuar con " + cantidad + " unidades?");
+        if (!confirmarCantidad) {
+            continue; // vuelve al inicio del bucle para seleccionar nuevamente
+        }
+    }
+
+    if (cantidad < 0) {
+        alert("No se pueden seleccionar cantidades negativas.");
+        continue; // vuelve al inicio del bucle para seleccionar nuevamente
+    }
+
     let prendaElegida = determinarPrenda(opcion);
 
     if (prendaElegida) {
-        // Calcular el subtotal de la compra
+        // calcular el subtotal de la compra
         let subtotal = 0;
         switch (prendaElegida) {
             case "vestido":
@@ -64,13 +79,14 @@ do {
                 subtotal = precioCartera * cantidad;
                 break;
         }
-        totalCompra += subtotal; // Agregar el subtotal al total de la compra
+        totalCompra += subtotal; // agregar el subtotal al total de la compra
         alert("Ha seleccionado " + cantidad + " " + prendaElegida + "(s). Subtotal: $" + subtotal);
     }
 
-   continuar = prompt("¿Desea seleccionar otras prendas? (Si/No)").toLowerCase();
+    continuar = prompt("¿Desea seleccionar otras prendas? (Si/No)").toLowerCase();
 
 } while (continuar === "si");
 
-// Mostrar el total de la compra
+// mostrar el total de la compra
 alert("El total de su compra es: $" + totalCompra + ". ¡Gracias por su compra!");
+
